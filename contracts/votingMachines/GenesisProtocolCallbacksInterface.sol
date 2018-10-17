fbbc77b40a80bc6097d3e480b2670b21a0515bbe
Ben Kaufman <ben.kaufman10@gmail.com>
2018-10-17 16:47:33 +0300

    Fixed solium error

--- contracts/votingMachines/GenesisProtocolCallbacksInterface.sol
+++ contracts/votingMachines/GenesisProtocolCallbacksInterface.sol
@@ -4,7 +4,7 @@ import "openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
 import "./VotingMachineCallbacksInterface.sol";
 
 
-contract GenesisProtocolCallbacksInterface is VotingMachineCallbacksInterface{
+contract GenesisProtocolCallbacksInterface is VotingMachineCallbacksInterface {
     function mintReputation(uint _amount,address _beneficiary,bytes32 _proposalId) external returns(bool);
     function burnReputation(uint _amount,address _owner,bytes32 _proposalId) external returns(bool);
     function stakingTokenTransfer(StandardToken _stakingToken,address _beneficiary,uint _amount,bytes32 _proposalId) external returns(bool);
